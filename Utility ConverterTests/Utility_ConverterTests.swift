f963ced34468cc3d14c50032934be0897051586a
chim_98 <chavividusha@gmail.com>
2021-02-10 12:07:39 +0530

    Initial Commit

---
+++ Utility ConverterTests/Utility_ConverterTests.swift
@@ -0,0 +1,33 @@
+//
+//  Utility_ConverterTests.swift
+//  Utility ConverterTests
+//
+//  Created by Chaveen Ellawela on 2021-02-10.
+//
+
+import XCTest
+@testable import Utility_Converter
+
+class Utility_ConverterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
