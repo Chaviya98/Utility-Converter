ab2f3567ed53322989cbcb32b5f04b4f28fd84c8
chim_98 <chavividusha@gmail.com>
2021-02-14 07:23:15 +0530

    Localizable.strings file added.

--- Utility Converter/Controller/HelpAboutController.swift
+++ Utility Converter/Controller/HelpAboutController.swift
@@ -29,8 +29,8 @@ class HelpAboutController: UIViewController {
         self.headerImageView.layer.cornerRadius = 30
         self.headerImageView.clipsToBounds = true
         
-        self.primaryTextArea.text = "\u{2022} Utility Converter App supports speed conventions such as metres/sec, km/hour, miles/hour, nautical miles/hour (knot)."
-        self.secondaryTextArea.text = "\u{2022} You can save up to 5 conventions at a time and the history option will list only the last 5 conversions per category."
+        self.primaryTextArea.text = NSLocalizedString("SpeedHelpTextSectionA", comment: "")
+        self.secondaryTextArea.text = NSLocalizedString("SpeedHelpTextSectionB", comment: "")
     }
     
     @IBAction func headerNavBar(_ sender: Any) {
@@ -38,29 +38,29 @@ class HelpAboutController: UIViewController {
         if(headerNavBarSegementController.selectedSegmentIndex == 0)
         {
             headerImageView.image = UIImage(named: "SpeedHome")
-            primaryTextArea.text = "\u{2022} Utility Converter App supports speed conventions such as metres/sec, km/hour, miles/hour, nautical miles/hour (knot)."
-            secondaryTextArea.text = "\u{2022} You can save up to 5 conventions at a time and the history option will list only the last 5 conversions per category."
+            primaryTextArea.text = NSLocalizedString("SpeedHelpTextSectionA", comment: "")
+            secondaryTextArea.text = NSLocalizedString("SpeedHelpTextSectionB", comment: "")
         }
         else if(headerNavBarSegementController.selectedSegmentIndex == 1)
         {
             headerImageView.image = UIImage(named: "LengthHome")
-            primaryTextArea.text = "\u{2022} Utility Converter App supports length conventions such as metre, km, mile, cm, mm, yard, inch."
-            secondaryTextArea.text = "\u{2022} You can save up to 5 conventions at a time and the history option will list only the last 5 conversions per category."
+            primaryTextArea.text = NSLocalizedString("LengthHelpTextSectionA", comment: "")
+            secondaryTextArea.text = NSLocalizedString("SpeedHelpTextSectionB", comment: "")
         }else if(headerNavBarSegementController.selectedSegmentIndex == 2)
         {
             headerImageView.image = UIImage(named: "TemperatureHome")
-            primaryTextArea.text = "\u{2022} Utility Converter App supports temperature conventions such as Celsius, Fahrenheit, Kelvin."
-            secondaryTextArea.text = "\u{2022} You can save up to 5 conventions at a time and the history option will list only the last 5 conversions per category."
+            primaryTextArea.text = NSLocalizedString("TemperatureHelpTextSectionA", comment: "")
+            secondaryTextArea.text = NSLocalizedString("SpeedHelpTextSectionB", comment: "")
         }else if(headerNavBarSegementController.selectedSegmentIndex == 3)
         {
             headerImageView.image = UIImage(named: "VolumeHome")
-            primaryTextArea.text = "\u{2022} Utility Converter App supports volume convention such as UK gallon, litre, UK pint, fluid ounce, millilitre."
-            secondaryTextArea.text = "\u{2022} You can save up to 5 conventions at a time and the history option will list only the last 5 conversions per category."
+            primaryTextArea.text = NSLocalizedString("VolumeHelpTextSectionA", comment: "")
+            secondaryTextArea.text = NSLocalizedString("SpeedHelpTextSectionB", comment: "")
         }else if(headerNavBarSegementController.selectedSegmentIndex == 4)
         {
             headerImageView.image = UIImage(named: "WeightHome")
-            primaryTextArea.text = "\u{2022} Utility Converter App supports weight conventions such as Kg, grams, ounces, pounds, stone-pounds."
-            secondaryTextArea.text = "\u{2022} You can save up to 5 conventions at a time and the history option will list only the last 5 conversions per category."
+            primaryTextArea.text = NSLocalizedString("WeightHelpTextSectionA", comment: "")
+            secondaryTextArea.text = NSLocalizedString("SpeedHelpTextSectionB", comment: "")
         }
     }
 }
